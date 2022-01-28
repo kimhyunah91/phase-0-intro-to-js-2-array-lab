437bcdbdbc40be886e24b6081da2fb495a170eb3
Christine Kim <kimhyunah1991@gmail.com>
2022-01-27 20:56:47 -0500

    commit tree 453c8c0e61a71347017c4bf672899561a886d528

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = [
+  "Milo",
+  "Otis",
+  "Garfield"
+]
+
+function destructivelyAppendCat(Ralph) {
+  cats.push(Ralph)
+  return cats
+}
+function destructivelyPrependCat(Bob) {
+  cats.unshift(Bob)
+  return cats
+}
+function destructivelyRemoveLastCat(Garfield) {
+  cats.pop(Garfield)
+  return cats
+}
+function destructivelyRemoveFirstCat(Milo) {
+  cats.shift(Milo)
+  return cats
+}
+function appendCat(Broom) {
+  const newcats = [...cats, Broom]
+  return newcats
+}
+function prependCat(Arnold) {
+  const addCats = [Arnold, ...cats]
+  return addCats
+}
+function removeLastCat(Milo, Otis){
+  const goneCats = cats.slice (0, 2)
+  return goneCats
+}
+function removeFirstCat(Otis, Garfield) {
+  const letsGoCats = cats.slice(1)
+  return letsGoCats
+}
